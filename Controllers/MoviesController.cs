e738470e46b81d1130d5f1daca66d7488e99bfbc
TrevorSlobodnickSCC <W0740591@myscc.ca>
2021-09-27 14:53:39 -0400

    Part 5 Complete

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
 using MvcMovie.Models;
 
 namespace MvcMovie.Controllers
